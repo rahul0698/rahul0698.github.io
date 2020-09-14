db3bec533070078ea2e136c43d242c076de8ecbd
Rahul Raj Chakravorty <rahuliitdh16@gmail.com>
2020-09-14 12:38:50 +0530

    build

---
+++ build/precache-manifest.948c08edec36e323abb70a2ee3d9b64f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ce2fd643523942483883429f8e87d29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48d4da225bbb87c1819b",
+    "url": "/static/css/main.8579c196.chunk.css"
+  },
+  {
+    "revision": "500970c4f6a10a20683a",
+    "url": "/static/js/2.be78221e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.be78221e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48d4da225bbb87c1819b",
+    "url": "/static/js/main.244fbc58.chunk.js"
+  },
+  {
+    "revision": "efe25b4b8d1f7af9da4b",
+    "url": "/static/js/runtime-main.6b3488d1.js"
+  },
+  {
+    "revision": "12f2b20e816b313c2486aace7f480a87",
+    "url": "/static/media/basset-and-gold-logo-white-svg.12f2b20e.svg"
+  },
+  {
+    "revision": "0761a4dfd60f7043f73542f93a5ce1ed",
+    "url": "/static/media/cubid_logo.0761a4df.png"
+  },
+  {
+    "revision": "430db85a2fb4f464787ebbaf6aeb31a1",
+    "url": "/static/media/demo_image.430db85a.jpg"
+  },
+  {
+    "revision": "10cc2dc031c3f0ba4da28a5312c4f444",
+    "url": "/static/media/home.10cc2dc0.jpg"
+  },
+  {
+    "revision": "88102d2a7283d65ef90a216c743e0de5",
+    "url": "/static/media/profile_pic.88102d2a.jpg"
+  },
+  {
+    "revision": "96e3e265083c9180c93deff4b944a5d8",
+    "url": "/static/media/udi.96e3e265.jpeg"
+  },
+  {
+    "revision": "5c0d66e94d7e19f246a08ff770f47243",
+    "url": "/static/media/wisdo.5c0d66e9.svg"
+  }
+]);
